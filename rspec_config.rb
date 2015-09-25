e49f3203aeeae11cae0a362f77041b0d624b9cae
James Brooks <jhbrooks@uchicago.edu>
2015-09-25 15:00:52 -0400

    changed rspec_config.rb to remove deprecation warnings from should syntax

--- rspec_config.rb
+++ rspec_config.rb
@@ -1,4 +1,10 @@
 RSpec.configure do |c|
   c.fail_fast = true
   c.color = true
+  c.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  c.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
