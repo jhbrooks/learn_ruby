f084d8174325d64ad7c6635266caec50a904894f
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 01:07:19 -0400

    fixed building of u_vowels and u_consonants

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,10 @@
 def translate_word (s, cons_buff='')
 	vowels = ["a","e","i","o","u"]
 	consonants = ["b","c","d"] + ["f","g","h"] + ["j","k","l","m","n"] + ["p","q","r","s","t"] + ["v","w","x","y","z"]
-	u_vowels = vowels.each { |v| v.upcase }
-	u_consonants = consonants.each { |c| c.upcase }
+	u_vowels = []
+	vowels.each { |v| u_vowels << v.upcase }
+	u_consonants = []
+	consonants.each { |c| u_consonants << c.upcase }
 	if (vowels.include?(s[0]) || u_vowels.include?(s[0])) && !((s[0] == "u" || s[0] == "U") && cons_buff != '')
 		return s + cons_buff + "ay"
 	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0])) || ((s[0] == "u" || s[0] == "U") && cons_buff != '')
