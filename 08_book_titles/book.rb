302269b92998fe2d86283646186788d0798fa896
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 13:51:44 -0400

    copied titleize method from simon_says.rb, redefined title= as @title = titleize(title)

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,7 +1,18 @@
 class Book
 	attr_reader :title
 
+	def titleize (s)
+		is_first = true
+		title_array = s.split(' ').each do |word|
+			unless (["a","an","the","in","on","to","of","or","and","by","for","but","over","under"].include?(word) && (is_first == false))
+				word.capitalize!
+			end
+			is_first = false
+		end
+		title = title_array.join(' ')
+	end
+
 	def title= (title)
-		@title = title
+		@title = titleize(title)
 	end
 end
