df04f877a1c8f1300463601ab3153f43bea7c1e4
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 00:58:26 -0400

    added punctuation and capitalization specs; discovered that transforming words that start with capital letters is broken (status preservation issues aside)

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -69,4 +69,14 @@ describe "#translate" do
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
 
+  it "retains punctuation" do
+    s = translate("who, me? madness. madness!")
+    expect(s).to eq("owhay, emay? adnessmay. adnessmay!")
+  end
+
+  it "preserves capitalization status on transformed words" do
+    s = translate("What happened to George the Bruce?")
+    expect(s).to eq("Atwhay appenedhay otay Eorgegay ethay Ucebray?")
+  end
+
 end
