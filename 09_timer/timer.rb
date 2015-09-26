71650e758d70585bfef8a64fda4498236098ee8a
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 14:28:11 -0400

    defined time_string, using modular arithmetic and integer division on @seconds, running the results through padded, and interpolating them into a string

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -5,6 +5,13 @@ class Timer
 		@seconds = 0
 	end
 
+	def time_string
+		secs = padded(@seconds % 60) 
+		mins = padded((@seconds / 60) % 60)
+		hrs = padded((@seconds / 60) / 60)
+		"#{hrs}:#{mins}:#{secs}"
+	end
+
 	def padded (n)
 		if n < 10
 			return "0" + n.to_s
