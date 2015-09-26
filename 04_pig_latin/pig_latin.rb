950ff3acdb1770019458daa0e730b4120ed83644
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 00:13:30 -0400

    updated translate word to treat u and U like consonants if cons_buff is not empty

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,9 +3,9 @@ def translate_word (s, cons_buff='')
 	consonants = ["b","c","d"] + ["f","g","h"] + ["j","k","l","m","n"] + ["p","q","r","s","t"] + ["v","w","x","y","z"]
 	u_vowels = vowels.each { |v| v.upcase }
 	u_consonants = consonants.each { |c| c.upcase }
-	if (vowels.include?(s[0]) || u_vowels.include?(s[0]))
+	if (vowels.include?(s[0]) || u_vowels.include?(s[0])) && !((s[0] == "u" || s[0] == "U") && cons_buff != '')
 		return s + cons_buff + "ay"
-	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0]))
+	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0])) || ((s[0] == "u" || s[0] == "U") && cons_buff != '')
 		return translate_word(s[1...(s.length)], (cons_buff + s[0]))
 	else
 		return s
