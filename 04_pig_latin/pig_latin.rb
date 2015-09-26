3c3d8da3f212acb5e227bc389a882d923a3e217d
James Brooks <jhbrooks@uchicago.edu>
2015-09-25 23:23:23 -0400

    updated translate_word to handle words starting with a single consonant

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,10 @@
 def translate_word (s)
 	if ["a","e","i","o","u"].include?(s[0])
 		return s + "ay"
+	elsif (["b","c","d"] + ["f","g","h"] + ["j","k","l","m","n"] + ["p","q","r","s","t"] + ["v","w","x","y","z"]).include?(s[0])
+		return s[1...(s.length)] + s[0] + "ay"
+	else
+		return s
 	end
 end
 
