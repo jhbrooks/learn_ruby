d5498b051f3c2196db1fe9b5274bfae4abf77fb8
James Brooks <jhbrooks@uchicago.edu>
2015-09-26 01:52:51 -0400

    added is_cap parameter (default false) and an if clause that sets it to true if the first letter of the string is capitalized on the very first pass through translate_word (when cons_buff is ''); added is_cap handling in the vowel and consonant case; fixed 'qu' detection to not work as 'random-consonant-u' detection

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,27 @@
-def translate_word (s, cons_buff='')
+def translate_word (s, cons_buff='', is_cap='false')
+	if (cons_buff == '') && (s == s.capitalize)
+		is_cap = true
+	end
+
 	vowels = ["a","e","i","o","u"]
 	consonants = ["b","c","d"] + ["f","g","h"] + ["j","k","l","m","n"] + ["p","q","r","s","t"] + ["v","w","x","y","z"]
 	u_vowels = []
 	vowels.each { |v| u_vowels << v.upcase }
 	u_consonants = []
 	consonants.each { |c| u_consonants << c.upcase }
-	if (vowels.include?(s[0]) || u_vowels.include?(s[0])) && !((s[0] == "u" || s[0] == "U") && cons_buff != '')
-		return s + cons_buff + "ay"
-	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0])) || ((s[0] == "u" || s[0] == "U") && cons_buff != '')
-		return translate_word(s[1...(s.length)], (cons_buff + s[0]))
+
+	if (vowels.include?(s[0]) || u_vowels.include?(s[0])) && !((s[0] == "u" || s[0] == "U") && (cons_buff != '' && (cons_buff[-1,1] == 'q' || cons_buff[-1,1] == 'Q')))
+		if is_cap == true
+			return s[0].upcase + s[1...(s.length)] + cons_buff + "ay"
+		else
+			return s + cons_buff + "ay"
+		end
+	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0])) || ((s[0] == "u" || s[0] == "U") && (cons_buff != '' && (cons_buff[-1,1] == 'q' || cons_buff[-1,1] == 'Q')))
+		if is_cap == true
+			return translate_word(s[1...(s.length)], (cons_buff + s[0].downcase), is_cap)
+		else
+			return translate_word(s[1...(s.length)], (cons_buff + s[0]), is_cap)
+		end
 	else
 		return s
 	end
