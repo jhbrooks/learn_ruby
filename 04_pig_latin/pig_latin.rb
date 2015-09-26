082fa8dbd6c771be67b24e86957bd5588301a097
James Brooks <jhbrooks@uchicago.edu>
2015-09-25 23:46:03 -0400

    added a consonant buffer parameter to translate word, with a default value of ''; the vowel case now tacks on cons_buff before the 'ay', while the consonant case calls translate_word with the consonant shifted from s to cons_buff

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,12 @@
-def translate_word (s)
+def translate_word (s, cons_buff='')
 	vowels = ["a","e","i","o","u"]
 	consonants = ["b","c","d"] + ["f","g","h"] + ["j","k","l","m","n"] + ["p","q","r","s","t"] + ["v","w","x","y","z"]
 	u_vowels = vowels.each { |v| v.upcase }
 	u_consonants = consonants.each { |c| c.upcase }
 	if (vowels.include?(s[0]) || u_vowels.include?(s[0]))
-		return s + "ay"
+		return s + cons_buff + "ay"
 	elsif (consonants.include?(s[0]) || u_consonants.include?(s[0]))
-		return s[1...(s.length)] + s[0] + "ay"
+		return translate_word(s[1...(s.length)], (cons_buff + s[0]))
 	else
 		return s + "ay"
 	end
